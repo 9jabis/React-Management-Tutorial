3d482a9ded9288ccc0e65443d644734bfccc13c8
9jabis <90185128+9jabis@users.noreply.github.com>
2023-11-27 16:45:59 +0900

    Crate Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://picsum.photos/64/64/',
+  'name': '구자빈',
+  'birthday': '990401',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://picsum.photos/64/64//',
+  'name': '구자빈2',
+  'birthday': '990402',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 3,
+  'image': 'https://picsum.photos/64/64///',
+  'name': '구자빈3',
+  'birthday': '990403',
+  'gender': '남자',
+  'job': '대학생'
+}
+]
+
 
 class App extends Component {
   render() {
     return (
-      <div className='gray-background'>
-        <img src={logo} let='logo' />
-        <h2>management sysdtem</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                job={c.job}
+              />
+            )
+          })
+        }
       </div>
     );
   }
